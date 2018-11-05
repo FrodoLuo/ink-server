1af9d730824882c0fa7a59e3ae4987f759cca17a
Luo Yuzhou <frodoluo@outlook.com>
2018-11-05 11:37:43 +0800

    update database configs

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,7 +5,7 @@ export const databaseConfig: TypeOrmModuleOptions =  {
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'LUOYUZHOU_1996',
+  password: 'LuoyuzhoU_1996',
   database: 'ink',
   entities: ['src/entity/*.entity{.ts,.js}'],
   synchronize: true,
