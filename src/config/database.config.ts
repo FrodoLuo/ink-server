ee59c1cacb6d8a68f559ecef19343112ec807f8d
FrodoLuo <frodoluo@outlook.com>
2018-11-05 20:57:11 +0800

    add a prod env diff for config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-export const databaseConfig: TypeOrmModuleOptions =  {
+export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: 'root',
   password: 'LuoyuzhoU_1996',
   database: 'ink',
-  entities: ['src/entity/*.entity{.ts,.js}'],
+  entities: [
+    process.env.NODE_ENV === 'production' ?
+      'dist/entity/*.entity{.ts,.js}'
+      :
+      'src/entity/*.entity{.ts,.js}',
+  ],
   synchronize: true,
 };
